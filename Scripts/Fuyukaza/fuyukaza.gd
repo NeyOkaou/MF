fb1d8c16431f53e9da3ccc953fdf99fcee7bb38c
NeyOkaou <143295893+NeyOkaou@users.noreply.github.com>
2025-06-01 00:55:34 +0200

    Base game 2

--- Scripts/Fuyukaza/fuyukaza.gd
+++ Scripts/Fuyukaza/fuyukaza.gd
@@ -2,27 +2,34 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -700.0
+var double_jump = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") + 1000
 
 
 func _physics_process(delta):
+	velocity.x = 0
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	
+	if Input.is_action_just_pressed("UP") and double_jump:
 		velocity.y = JUMP_VELOCITY
+		double_jump = false
+	if is_on_floor():
+		double_jump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#return -1 and 1 on left or right
+	if Input.is_action_pressed("LEFT"):
+		$Sprite2D.flip_h = true
+		velocity.x = -SPEED
+	if Input.is_action_pressed("RIGHT"):
+		$Sprite2D.flip_h = false
+		velocity.x = SPEED
 	move_and_slide()
